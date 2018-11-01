b1f984e7fe0f940e397cde786f4ee2a4b7158b6c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-01 14:41:37 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def turn_count(board)
+  total = 0
+  board.each do |entry|
+    if !(entry == "   " || entry == " ")
+      total += 1
+    end
+  end
+  total
+end
+
+def current_player(board)
+  player = turns % 2 == 0 ? "X" : "O"
+  return player
+end
+  
